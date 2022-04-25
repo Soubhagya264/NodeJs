a1a7a2b241c3936b564ce99542d4d9f3d3b2cd07
Soubhagya264 <71813414+Soubhagya264@users.noreply.github.com>
2022-04-25 14:08:25 +0530

    Node Js Basic start

---
+++ index.js
@@ -0,0 +1,11 @@
+// const os =require('os');
+// console.log(os.type());
+
+const car={
+    make: 'Honda',
+    model: 'Civic',
+    year: '2000',
+}
+
+console.log(car.make);
+module.exports=car;
---
+++ magic.js
@@ -0,0 +1,4 @@
+const car = require('./index.js');
+console.log(car.make);
+console.log(car.model);
+console.log(car.year);
