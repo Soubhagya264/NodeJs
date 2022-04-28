8210f39bfd16d65deb6da02459268fa73167f8c1
Soubhagya264 <71813414+Soubhagya264@users.noreply.github.com>
2022-04-28 22:21:48 +0530

    express form

--- ExpressJs/Express-App/server.js
+++ ExpressJs/Express-App/server.js
@@ -1,10 +1,31 @@
 const express = require('express');
 const app = express();
+const path=require('path');
+
 app.set('view engine', 'pug');
+app.use(express.urlencoded({extended: true}));
 
 app.get('/', (req, res) => {
     res.render('index');
 }
+);
+
+app.post('/form_submit', (req, res) => {
+   
+    const name = req.body.username;
+    const email = req.body.email;
+    const message = req.body.message;
+    console.log(name);
+    console.log(email);
+    console.log(message);
+    res.end('USER NAME ' + name + 'EMAIL ' + email + 'MESSAGE ' + message);
+
+
+    
+}
+
+
+
 ).listen(3000, () => {
     console.log('App listening on port 3000!');
 }
